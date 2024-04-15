51af74b736672e47b5be6779009e40a9c1297ce6
hemantDwivedi <hemantdwiwedi@gmail.com>
2024-04-15 20:38:29 +0530

    optimize application exception handler

--- src/main/java/com/xyz/newsletterbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/xyz/newsletterbackend/exception/GlobalExceptionHandler.java
@@ -4,32 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-
-import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<ErrorDetails> handleApiException(ApiException exception, WebRequest request){
+    public ResponseEntity<ErrorDetails> handleApiException(ApiException exception){
         return new ResponseEntity<>(
                 new ErrorDetails(
-                        LocalDateTime.now(),
                         exception.getMessage(),
-                        request.getDescription(false)
-                )
-                , HttpStatus.BAD_REQUEST
-        );
-    }
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest request){
-        return new ResponseEntity<>(
-                new ErrorDetails(
-                        LocalDateTime.now(),
-                        exception.getMessage(),
-                        request.getDescription(false)
+                        exception.getStatus()
                 )
                 , HttpStatus.BAD_REQUEST
         );
--- src/main/java/com/xyz/newsletterbackend/service/EmailSenderService.java
+++ src/main/java/com/xyz/newsletterbackend/service/EmailSenderService.java
@@ -1,9 +1,11 @@
 package com.xyz.newsletterbackend.service;
 
 import com.xyz.newsletterbackend.connection.MailSender;
+import com.xyz.newsletterbackend.exception.ApiException;
 import com.xyz.newsletterbackend.model.MailRequest;
 import com.xyz.newsletterbackend.utils.EmailDetailsValidator;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
@@ -15,7 +17,7 @@ public class EmailSenderService {
 
     public void newMail(MailRequest mailRequest){
         if (emailDetailsValidator.isSubscriptionValid(mailRequest.getTargetEmail())){
-            throw new RuntimeException("name and email violating");
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Email violating");
         }
         mailSenderHandler(mailRequest);
     }
@@ -24,7 +26,7 @@ public class EmailSenderService {
         try {
             MailSender.sendEmail(mailRequest.getTargetEmail(), mailRequest.getMessage());
         } catch (MessagingException e) {
-            throw new RuntimeException(e);
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Service unavailable");
         }
     }
 }
